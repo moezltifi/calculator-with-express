81295cf97d0047b70b0154a1a4f6b82bd6252733
moezl <moez.mltifi@gmail.com>
2024-04-13 14:08:12 +0100

    responding to requests with html files

--- calculator.js
+++ calculator.js
@@ -3,7 +3,7 @@ const express = require("express")
 const app =express()
 
 app.get("/", function(req, res){
-    res.send("hi")
+    res.sendFile(__dirname + "/index.html")
 })
 
 app.listen(3000,function(){
